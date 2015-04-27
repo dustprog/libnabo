ad4650af732b615aeba94dd6f3e1d4c36b8f0f26
dustprog <dustprog@gmail.com>
2015-04-27 08:24:23 -0500

    kdtree cpp implementation for CUDA

---
+++ nabo/kdtree_cuda.cpp
@@ -0,0 +1,32 @@
+// CUDA kdtree implementation cpp file
+// http://stackoverflow.com/questions/2090974/how-to-separate-cuda-code-into-multiple-files
+
+#ifdef HAVE_CUDA
+
+#include "nabo_private.h"
+#include "index_heap.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <stdexcept>
+#include <limits>
+#include <queue>
+#include <algorithm>
+// #include <map>
+#include <boost/numeric/conversion/bounds.hpp>
+#include <boost/limits.hpp>
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
+
+namespace Nabo
+{
+	template<typename T>
+	CUDASearch<T>::CUDASearch(const Matrix& cloud, const Index dim, const unsigned creationOptionFlags):
+		NearestNeighbourSearch<T>::NearestNeighbourSearch(cloud, dim, creationOptionFlags),
+		//deviceType(deviceType),
+		//context(contextManager.createContext(deviceType))
+	{
+	}
+	
+	
+};
